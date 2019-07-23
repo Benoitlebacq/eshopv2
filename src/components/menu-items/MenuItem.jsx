dafaffc6d313a1fed1100da6f60bf502f9625022
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-07-23 15:48:14 +0200

    homepage with scale effect ok

---
+++ src/components/menu-items/MenuItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./menuItem-style.scss";
+
+const MenuItem = ({ title, image, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${image})`
+        }}
+      />
+      <div className="content">
+        <h2 className="title">{title.toUpperCase()}</h2>
+        <span className="subtitle">Shop now </span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
