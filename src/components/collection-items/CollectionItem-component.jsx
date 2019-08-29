ac5ce82b68c12eb79529981ecff070312dd012f8
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-08-29 14:35:50 +0200

    git testing , issues

--- src/components/collection-items/CollectionItem-component.jsx
+++ src/components/collection-items/CollectionItem-component.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { connect } from 'react-redux'
+import { addItem } from '../../redux/cart/cart-action'
 import "./collectionItem-style.scss";
+import CustomButton from "../CustomButton-component";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const {name, price, imageUrl} = item
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,8 +14,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton onClick={()=> addItem(item)}>Add to Cart</CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch  => ({
+  addItem : item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps) (CollectionItem);
