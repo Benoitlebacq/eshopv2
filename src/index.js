41cf633e4ca687174d9be0344b086f0a396b33bc
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-09-02 11:51:12 +0200

    added persistor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux'
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import store from './redux/store'
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </Provider>
-  ,
-
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
--- src/redux/cart/cart-action.js
+++ src/redux/cart/cart-action.js
@@ -1,9 +1,13 @@
 export const toggleCartHidden = () => ({
-    type : 'TOGGLE_CART_HIDDEN'
-})
+  type: "TOGGLE_CART_HIDDEN"
+});
 
+export const addItem = item => ({
+  type: "ADD_ITEM",
+  payload: item
+});
 
-export const addItem = (item) => ({
-    type : 'ADD_ITEM',
-    payload : item
-})
+export const clearItemFromCart = item => ({
+  type: "CLEAR_ITEM_FROM_CART",
+  payload: item
+});
