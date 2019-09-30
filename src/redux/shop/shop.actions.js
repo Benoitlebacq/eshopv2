8e50524bc53f0be97732dcb9be4a20afdf3adf65
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-09-30 13:27:26 +0200

    wrapping spinning render in antoher way

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,4 +1,31 @@
-export const updateCollections = collectionsMap => ({
-  type: "UPDATE_COLLECTIONS",
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = collectionsMap => ({
+  type: "FETCH_COLLECTIONS_START"
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: "FETCH_COLLECTIONS_SUCCESS",
   payload: collectionsMap
 });
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: "FETCH_COLLECTIONS_FAILURE",
+  payload: errorMessage
+});
+
+export const fetchCollectionsStartAsync = () => dispatch => {
+  const collectionRef = firestore.collection("collection");
+  dispatch(fetchCollectionsStart());
+
+  collectionRef
+    .get()
+    .then(snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      dispatch(fetchCollectionsSuccess(collectionsMap));
+    })
+    .catch(err => dispatch(fetchCollectionsFailure(err.message)));
+};
